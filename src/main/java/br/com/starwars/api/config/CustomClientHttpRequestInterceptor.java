d5639963ccd592e0c92e833e4c00c0a56fa6e15a
Gabriel <gabrielmqs04@gmail.com>
2019-06-03 13:29:40 -0300

    atualizando interceptor

--- src/main/java/br/com/starwars/api/config/CustomClientHttpRequestInterceptor.java
+++ src/main/java/br/com/starwars/api/config/CustomClientHttpRequestInterceptor.java
@@ -8,12 +8,16 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Component
 public class CustomClientHttpRequestInterceptor implements ClientHttpRequestInterceptor {
 
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
 			throws IOException {
+		log.info("[{}] - {}", request.getMethod().name().toUpperCase(), request.getURI().toString());
 		return execution.execute(request, body);
 	}
 
