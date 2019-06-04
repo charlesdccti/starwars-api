0d22dcc0b78675bd93c245a164cc270c8f158e6b
Gabriel <gabrielmqs04@gmail.com>
2019-06-03 23:47:17 -0300

    atualizando exception

--- src/main/java/br/com/starwars/api/exception/PlanetaComNomeDuplicadoException.java
+++ src/main/java/br/com/starwars/api/exception/PlanetaComNomeDuplicadoException.java
@@ -3,7 +3,7 @@ package br.com.starwars.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PlanetaComNomeDuplicadoException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
