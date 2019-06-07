aa676876448f3cccb36b760c7b71e06a01a28d63
Gabriel <gabrielmqs04@gmail.com>
2019-06-07 08:39:11 -0300

    adicionando dependencia e habilitando cache

--- src/main/java/br/com/starwars/api/StarwarsApiApplication.java
+++ src/main/java/br/com/starwars/api/StarwarsApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 
 import br.com.starwars.api.config.RestTemplateBean;
 
+@EnableCaching
 @SpringBootApplication(exclude=EmbeddedMongoAutoConfiguration.class)
 public class StarwarsApiApplication {
 
